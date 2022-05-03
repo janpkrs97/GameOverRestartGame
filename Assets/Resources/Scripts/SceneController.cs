94d963a8bfbc7177a95611e15f3ba50d5007f125
janpkrs97 <57917997+janpkrs97@users.noreply.github.com>
2022-05-03 11:41:06 +0200

    Completed Menu Scene

---
+++ Assets/Resources/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ChangeScene(int classID)
+    {
+        if (classID == 1)
+        {
+            SceneManager.LoadScene("Standard");
+        }
+        else if (classID == 2)
+        {
+            SceneManager.LoadScene("Circle");
+        }
+    }
+}
